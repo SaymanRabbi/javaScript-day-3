c3d42a3890fe9b0f19dce04c30f799c3a72bd5cc
sayman <mdrabbi.asm@gmail.com>
2022-01-27 09:58:25 +0600

    and or operator

--- chlanges.js
+++ chlanges.js
@@ -31,17 +31,28 @@
 // };
 // তাড়াহুড়া করে স্কুলের জন্য বের হচ্ছ কিন্তু রাস্তা পার হওয়ার সময় দেখলে, ট্রাফিক সিগন্যাল লাল রং। এই অবস্থায় যদি তুমি রাস্তা পার হওয়ার চেষ্টা কর, তাহলে ডেঞ্জার হতে পারে। যদি হলুদ রং হয় তাহলে তোমার থেমে যাওয়া উচিত। আর যদি ট্রাফিক সিগন্যাল গ্রিন হয় তাহলে তোমার রাস্তা পার হওয়া উচিত। তাই একটা কোড লিখে ফেলো। যেখানে আমরা signal নামে একটা ভেরিয়েবল থাকবে। সেটার মান green, yellow বা red হতে পারে। সেই অনুসারে ডিফারেন্ট ডিফারেন্ট কাজ হবে। তো, সেই কোড ফটাফট লিখে ফেলো। 
 
-var singel = 'white';
+// var singel = 'white';
 
-if (singel == 'red'){
-    console.log("Its Danger Don't Go");
-}
-else if (singel == 'yello'){
-    console.log("You Sholud Stop Now");
-}
-else if (singel == 'green'){
-    console.log("You Have To Go Now")
-}
-else {
-    console.log("BalPakna Tor Jaowr Dorkar Nai")
-};
+// if (singel == 'red'){
+//     console.log("Its Danger Don't Go");
+// }
+// else if (singel == 'yello'){
+//     console.log("You Sholud Stop Now");
+// }
+// else if (singel == 'green'){
+//     console.log("You Have To Go Now")
+// }
+// else {
+//     console.log("BalPakna Tor Jaowr Dorkar Nai")
+// };
+// -----or oparetor------
+
+// var isTrue = 5<6;
+// var isFalse = 5>6;
+// var check = isTrue || isFalse;
+// console.log(check);
+// ------ and oparetor-----
+var isTrue = 5<6;
+var isFalse = 7>6;
+var check = isTrue && isFalse;
+console.log(check);
